014395fed4ee358edd1e86f2f3ff01247bdedab7
Anu Shibin Joseph Raj <anushibin007@gmail.com>
2026-03-25 10:32:08 +0530

    ✨ Allow different base for vite

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -3,7 +3,8 @@ set -e
 
 # Replace the compile-time sentinel with the actual backend URL at container startup.
 # This allows a single pre-built image to target different backends without rebuilding.
-find /usr/local/apache2/htdocs/assets -name '*.js' -exec \
+# Search all of htdocs so it works regardless of VITE_BASE_PATH sub-directory
+find /usr/local/apache2/htdocs -name '*.js' -exec \
   sed -i "s|__VITE_API_BASE_URL__|${VITE_API_BASE_URL}|g" {} \;
 
 exec "$@"
